8a01ace2ed1871e2390b4d7038f2ad5815f6027e
Gaston <gastonnus@gmail.com>
2020-02-26 23:13:51 +0800

    Onto display player score

--- Block_Breaker/Block Breaker/Assets/Scripts/Block.cs
+++ Block_Breaker/Block Breaker/Assets/Scripts/Block.cs
@@ -18,12 +18,15 @@ public class Block : MonoBehaviour
     // Script here is to destroy object
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        DestoryBlock();
+    }
+
+    private void DestoryBlock()
+    {
         // This function creates an audio source but automatically disposes of it once the clip has finished playing
         // Input parameters are AudioClip and Vector3 position which is source of audio
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
-        
-
+        level.BlockDestroyed();
     }
-
 }
--- Block_Breaker/Block Breaker/Assets/Scripts/Level.cs
+++ Block_Breaker/Block Breaker/Assets/Scripts/Level.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class Level : MonoBehaviour
 {
+    // Cached Component Reference
+    SceneLoader sceneloader;
+
+    private void Start()
+    {
+        sceneloader = FindObjectOfType<SceneLoader>();
+    }
+
     [SerializeField] int breakableBlocks; //Just for debugging purposes
     public void CountBreakableBlocks()
     {
         breakableBlocks++;
     }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneloader.LoadNextScene();
+        }
+    }
+
 }

6e3573af08bef7a93a06162161d81d8d4c5daf5a
gluonnus@hotmail.com <gluonnus@hotmail.com>
2020-02-27 15:51:32 +0800

    Able to display player score

--- Block_Breaker/Block Breaker/Assets/Scripts/Block.cs
+++ Block_Breaker/Block Breaker/Assets/Scripts/Block.cs
@@ -8,25 +8,28 @@ public class Block : MonoBehaviour
     [SerializeField] AudioClip breakSound;
     // Cached Component Reference
     Level level;
+    GameStatus gameStatus;
 
     private void Start()
     {
         level = FindObjectOfType<Level>();
         level.CountBreakableBlocks();
+        gameStatus = FindObjectOfType<GameStatus>();
     }
 
     // Script here is to destroy object
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        DestoryBlock();
+        DestroyBlock();
     }
 
-    private void DestoryBlock()
+    private void DestroyBlock()
     {
         // This function creates an audio source but automatically disposes of it once the clip has finished playing
         // Input parameters are AudioClip and Vector3 position which is source of audio
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
         level.BlockDestroyed();
+        gameStatus.AddToScore();
     }
 }
--- Block_Breaker/Block Breaker/Assets/Scripts/GameStatus.cs
+++ Block_Breaker/Block Breaker/Assets/Scripts/GameStatus.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
+    // Config Parameters
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
+    // State Variables
+    [SerializeField] int currentScore = 0;
+
+    private void Start()
     {
-        
+        scoreText.text = currentScore.ToString();
     }
 
     // Update is called once per frame
@@ -16,6 +22,12 @@ public class GameStatus : MonoBehaviour
     {
         // Time.timeScale to scale proportionally to realtime where 0.5 is 2x slower
         Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
     }
 
 

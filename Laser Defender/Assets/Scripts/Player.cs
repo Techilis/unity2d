ed8a04e9e6c3ebe3ada7c917a23756b9241979d2
Gaston <gastonnus@gmail.com>
2020-03-02 23:36:23 +0800

    Learnt about coroutine Coroutine - a function that allows time dilation so that objects can be instantiated without every frame from update to prevent overclouding

--- Laser Defender/Assets/Scripts/Player.cs
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Configuration Params
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float padding = 0.5f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float ProjectileFiringPeriod = 0.1f;
+
+    Coroutine firingCoroutine;
 
     float xMin;
     float xMax;
@@ -18,6 +24,37 @@ public class Player : MonoBehaviour
         SetUpMoveBoundaries();
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            firingCoroutine = StartCoroutine(FireContinuously());
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(firingCoroutine);
+        }
+    }
+
+    IEnumerator FireContinuously()
+    {
+        while (true)
+        {
+            // Quaternion.identity is saying no need rotation
+            // instantiate a laserPrefab INTO a GameObject so that we can use it easily
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            yield return new WaitForSeconds(ProjectileFiringPeriod);
+        }
+    }
+
     // Restricts movement to camera where (1,1) is top right of screen and (0,0) is btm left of screen
     private void SetUpMoveBoundaries()
     {
@@ -26,13 +63,6 @@ public class Player : MonoBehaviour
         xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
         yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
         yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Move();
-        Debug.Log(xMin + " : " + xMax);
     }
 
     private void Move()
@@ -46,7 +76,7 @@ public class Player : MonoBehaviour
 
         var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin+padding, xMax-padding);
         var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin+padding, yMax-padding);
-        Debug.Log(deltaX);
         transform.position = new Vector2(newXPos, newYPos);
     }
+
 }

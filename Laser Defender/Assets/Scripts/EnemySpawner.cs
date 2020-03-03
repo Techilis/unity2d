a1a8db1a0d19677fbc8e5b11e8c24ee55310391b
Gaston <gastonnus@gmail.com>
2020-03-04 00:47:55 +0800

    Learnt about creating enemy waves Essentially, create a path prefab to store the number of waypoints (we will want to get the transform.position of these waypoints) create a waveConfig file to store a list of these paths (otherwise known as waves) in the waveConfig file, loop through paths in list of paths and run coroutine and for loop for spawning enemy to follow waypoints for given number of enemies

---
+++ Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // Config Params
+    [SerializeField] List<WaveConfig> waveConfigs;
+    int startingWave = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currentWave = waveConfigs[startingWave];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    // Take note this is waveConfig type, NOT waveConfigs
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        for (int enemyCount = 0; enemyCount < waveConfig.GetNumberOfEnemies(); enemyCount++)
+        {
+            Instantiate(
+            waveConfig.GetEnemyPrefab(),
+            waveConfig.GetWaypoints()[0].transform.position,
+            Quaternion.identity);
+            // Quaternion.identity is just saying just use the rotation that the object started off with
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

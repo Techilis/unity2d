9b57e5652cd020610bba258fd54f01f824008ef9
Gaston <gastonnus@gmail.com>
2020-03-10 20:52:45 +0800

    Glitch Garden - create splash screen Load splash screen before loading to main menu

---
+++ Glitch Garden/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    int currentSceneIndex;
+    int timeToWait = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Grabbing buildIndex of whatever this scene is. buildIndex = 0 for Splash Screen
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}

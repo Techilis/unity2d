7a9529d025962dffbb7cbb9aac17702c5bdc01ca
Gaston <gastonnus@gmail.com>
2020-03-15 20:56:05 +0800

    Finishing Glitch Garden

---
+++ Glitch Garden/Assets/Scripts/AttackSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackSpawner : MonoBehaviour
+{
+    bool spawn = true;
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 5f;
+    [SerializeField] Attacker attackerPrefab;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(UnityEngine.Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Attacker newAttacker = Instantiate(attackerPrefab, transform.position, transform.rotation) as Attacker;
+        // instantiate the Attacker under 'Spawner (x)' GameObject as transform is pointing towards GameObject the spawned enemy
+        newAttacker.transform.parent = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Glitch Garden/Assets/Scripts/Shooter.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectile, gun;
+    AttackSpawner myLaneSpawner;
+    Animator animator;
+
+    // Can add input parameter which will appear as SerializeField in Unity event
+
+    private void Start()
+    {
+        SetLaneSpawner();
+        // because shooter is same level as Animator
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(IsAttackerInLane())
+        {
+            animator.SetBool("isAttacking", true);
+        }
+        else
+        {
+            animator.SetBool("isAttacking", false);
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackSpawner[] spawners = FindObjectsOfType<AttackSpawner>();
+        foreach (AttackSpawner spawner in spawners)
+        {
+            bool isCloseEnough = 
+                (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+            if (isCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public void Fire()
+    {
+        // Can use Quarternion.identity as opposed to transform.rotation
+        Instantiate(projectile, gun.transform.position, transform.rotation);
+    }
+}

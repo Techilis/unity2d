232322050e6242bc7e145d275dbe8f9b04f7db66
gluonnus@hotmail.com <gluonnus@hotmail.com>
2020-03-16 19:01:17 +0800

    Glitch Garden Updated

--- Glitch Garden/Assets/Scripts/Attacker.cs
+++ Glitch Garden/Assets/Scripts/Attacker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,8 @@ public class Attacker : MonoBehaviour
 {
     [Range(0f, 5f)] [SerializeField] float currentSpeed = 1f;
     // Start is called before the first frame update
+    GameObject currentTarget;
+
     void Start()
     {
         
@@ -15,10 +18,35 @@ public class Attacker : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector2.left * Time.deltaTime * currentSpeed);
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        if (!currentTarget)
+        {
+            GetComponent<Animator>().SetBool("isAttacking", false);
+        }
     }
 
     public void SetMovementSpeed(float speed)
     {
         currentSpeed = speed;
+    }
+
+    public void Attack(GameObject target)
+    {
+        GetComponent<Animator>().SetBool("isAttacking", true);
+        currentTarget = target;
+    }
+
+    public void StrikeCurrentTarget(float damage)
+    {
+        if (!currentTarget) { return; }
+        Health health = currentTarget.GetComponent<Health>();
+        if (health)
+        {
+            health.DealDamage(damage);
+        }
     }
 }
